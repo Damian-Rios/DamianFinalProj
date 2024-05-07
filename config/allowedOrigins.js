398c297ac2a46be5410a83eb4b6d0be14f72e0c9
Damian-Rios <damianjrios5@gmail.com>
2024-05-07 10:04:38 -0500

    adding proj link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://damianfinalproj.glitch.me/',
+    'https://damian-final-proj-30258a6d5ddb.herokuapp.com/',
     'http://localhost:5500',
     'http://localhost:3000',
     'http://127.0.0.1:5500'
